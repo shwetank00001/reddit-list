8dc03c7b354ad512a10116b8238547c63a37ce59
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-06-04 05:48:52 +0530

    Project setup done

---
+++ src/components/InputSubReddit.jsx
@@ -0,0 +1,43 @@
+import  { useEffect, useState } from 'react'
+
+const InputSubReddit = () => {
+    const [subreddit, setSubReddit] = useState('')
+    const url = `https://www.reddit.com/r/${subreddit}.json`;
+
+    const [dataFetched, setData]= useState([])
+    const [inputValue, setInputValue] = useState();
+
+    const handleAddReddit = (e) => {
+        e.preventDefault();
+        setSubReddit(inputValue);
+        console.log("The value of searched sub reddit is:", subreddit)
+        console.log(url)
+    }
+
+    useEffect(() => {
+        async function fetchReddit(){
+            const data = await fetch(url);
+            const resp = await data.json();
+            setData(resp.data.children.data);
+        }
+        fetchReddit()
+    }, [subreddit])
+
+    console.log("Fetched data", dataFetched);
+
+    const eleData = dataFetched.map(function(item){
+        return (
+            console.log(item.title)
+        )
+    })
+  return (
+    <div>
+        <h1>Enter the name of the subreddit</h1>
+        <input type='text' placeholder='enter' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={handleAddReddit}>Add Subreddit</button>
+        {eleData}
+    </div>
+  )
+}
+
+export default InputSubReddit
---
+++ src/components/SubRedditCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubRedditCard = () => {
+  return (
+    <div>SubRedditCard</div>
+  )
+}
+
+export default SubRedditCard
